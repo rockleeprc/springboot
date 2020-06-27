e839f64096b2d7bb9b6cd4eceda3adc92987375c
liyan <rockleeprc@outlook.com>
2020-06-27 20:36:18 +0800

    edit

--- spring-annotation-ioc/src/main/java/com/foo/processor/MyBeanFactoryPostProcessor.java
+++ spring-annotation-ioc/src/main/java/com/foo/processor/MyBeanFactoryPostProcessor.java
@@ -1,18 +1,15 @@
 package com.foo.processor;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
 
 public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println("MyBeanFactoryPostProcessor-->"+beanDefinitionName);
+            System.out.println("MyBeanFactoryPostProcessor-->" + beanDefinitionName);
         }
     }
 }
