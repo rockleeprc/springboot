fcc7644b244533aeb7bfd53b79c3ea2c57eadde7
rockleeprc <rockleeprc@outlook.com>
2021-01-18 14:35:38 +0800

    edit

--- springboot2/05_springmvc/src/main/java/com/example5/controller/ParamController.java
+++ springboot2/05_springmvc/src/main/java/com/example5/controller/ParamController.java
@@ -1,8 +1,5 @@
 package com.example5.controller;
 
-import org.springframework.core.DefaultParameterNameDiscoverer;
-import org.springframework.core.MethodParameter;
-import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +9,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.util.Arrays;
 
 @Validated
 @RestController
@@ -26,7 +20,7 @@ public class ParamController {
     }
 
     @RequestMapping("/p2")
-    public String param2(@Valid @NotBlank(message="str1不能为空") @RequestParam("str1") String str1, @NotNull @NotEmpty @RequestParam("str2") String str2) {
+    public String param2(@Valid @NotBlank(message="str1不能为空") @RequestParam("str1") String str1, @NotNull @RequestParam("str2") String str2) {
         return str1 + " | " + str2;
     }
 
