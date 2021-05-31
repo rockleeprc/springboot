8e966efb3c9c517a969488d4ec422e2c72a27a1a
rockleeprc <rockleeprc@outlook.com>
2021-05-31 17:05:24 +0800

    edit

--- spring-annotation-ioc/src/main/java/com/foo/config/CircularDependsOnConfiguration.java
+++ spring-annotation-ioc/src/main/java/com/foo/config/CircularDependsOnConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CircularDependsonConfiguration {
+public class CircularDependsOnConfiguration {
 
     @Bean
     public A a() {
